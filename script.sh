d15e42ba8e5ddc495b54761c0d5b792b34eaddf6
Placeholder6 <86405576+Placeholder6@users.noreply.github.com>
2021-08-14 23:16:35 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 while true
 do
     user="$( tr -dc A-Za-z0-9 </dev/urandom | head -c 5 )"
-    mono MinecraftClient.exe $user "-" HyperOt.aternos.me --Main.autorespawn=true &
+    mono MinecraftClient.exe $user "-" mc.spadebee.com --Main.autorespawn=true &
     sleep $( shuf -i 1-2 -n 1 )m
     pkill -f MinecraftClient.exe
     sleep $( shuf -i 1-2 -n 1 )m
